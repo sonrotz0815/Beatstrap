5af073063fcb786b2a993ed429ad88b1a0ede59e
Eric König <120642443+Sonrotz0815@users.noreply.github.com>
2023-12-25 16:04:09 +0100

    Bot can now join Voice Channels

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
 
+token="MTE4ODIwMTAyNjA1MDg3MTMwNg.GXA9BO.CxJXOOzNWmiOuklI5E6YvlYePMz3weA0EhwzO4"
+
 client = commands.Bot(command_prefix="-", intents=discord.Intents.all())
 
 @client.event
@@ -12,4 +14,13 @@ async def on_ready():
 async def test(ctx):
     await ctx.send("It works")
 
-client.run('MTE4ODIwMTAyNjA1MDg3MTMwNg.GXA9BO.CxJXOOzNWmiOuklI5E6YvlYePMz3weA0EhwzO4')
+@client.command(pass_context=True)
+async def play(ctx):
+    if(ctx.author.voice):
+        channel=ctx.author.voice.channel
+        await channel.connect()
+    else:
+        await ctx.send("Pls join a Voicechannel to use this command")
+
+
+client.run(token)
